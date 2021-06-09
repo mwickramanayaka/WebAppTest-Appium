02fae44b7122bc060fab2630cb488de484e32738
mwickramanayaka <manulwick@gmail.com>
2021-06-09 23:44:58 +0530

    desired capabilities

--- AndroidWebAppTest/src/test/java/tests/BaseClass.java
+++ AndroidWebAppTest/src/test/java/tests/BaseClass.java
@@ -1,24 +1,74 @@
 package tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class BaseClass {
 
+	static AppiumDriver<MobileElement> driver;
+
 	@BeforeTest
 	public void setUp() {
-		
-		DesiredCapabilities caps = new DesiredCapabilities();
-		
-		caps.setCapability("", "");
-		
+
+		try {
+
+			DesiredCapabilities caps = new DesiredCapabilities();
+
+			caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel");
+			caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+			
+			//caps.setCapability("platformName", "ANDROID");
+			//caps.setCapability(CapabilityType.PLATFORM_NAME, "ANDROID");
+			caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
+
+			//caps.setCapability(CapabilityType.VERSION, "9");
+			caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
+
+			caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
+
+			//APK path
+			//caps.setCapability(MobileCapabilityType.APP, "");
+
+			caps.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+
+			URL url = new URL("http:/127.0.0.1:4723/wd/hub");
+
+			driver = new AppiumDriver<MobileElement>(url, caps);
+			//driver = new AndroidDriver<MobileElement>(url, caps);
+			//driver = new IOSDriver<MobileElement>(url, caps);		
+
+		} catch (MalformedURLException e) {
+			System.out.println("The Cause is: " + e.getCause());
+			System.out.println("Message is: " + e.getMessage());
+			e.printStackTrace();
+
+		}
+
 	} 
+
+	@Test
+	public  void sampleTest() {
+		System.out.println("I'm inside the test");
+		
+	}
 	
 	@AfterTest
 	public void tearDown() {
-		
-		
+
+
 	} 
-	
+
 }
